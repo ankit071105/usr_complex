3e0e613e0d2c36c3cf2bdbe62274ffae86288f9a
Ankit Kumar <139707943+ankit071105@users.noreply.github.com>
2023-09-05 14:09:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
+
   
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('Booking Open Grab Your Shop Now....')
+  .typeString('UMA SHANKAR ROYAL COMPLEX')
+  .typeString('BOOKING OPEN GRAB YOUR SHOP NOW....')
+
   .pauseFor(200)
   .deleteChars(10)
   .start();
